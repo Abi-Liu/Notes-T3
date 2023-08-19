c4260c80c30729d4c335a41f7c25c0143292098e
Abi-Liu <abiliu018@gmail.com>
2023-08-18 17:56:05 -0700

    added topics route

---
+++ src/server/api/routers/topics.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const topicRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.topic.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+});
